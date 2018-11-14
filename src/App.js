4d6565b5eca5731481d1f73269e758025c383a7a
samagragupta <samagraguptajpr@gmail.com>
2018-11-14 17:33:15 +0530

    Added Text field

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Button } from "./components/Button";
+import { Input } from "./components/Input"
+
+// this.state{
+//   input=""
+// }
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <div className="wrapper">
-        <div className="row">
-          <Button> 7 </Button>
-          <Button> 8 </Button>
-          <Button> 9 </Button>
-        </div>
-        <div className="row">
-          <Button> 4 </Button>
-          <Button> 5 </Button>
-          <Button> 6 </Button>
-        </div>
-        <div className="row">
-          <Button> 1 </Button>
-          <Button> 2 </Button>
-          <Button> 3 </Button>
-        </div>
+          <Input>1</Input>
+          <div className="row">
+            <Button> 7 </Button>
+            <Button> 8 </Button>
+            <Button> 9 </Button>
+            <Button> / </Button>
+          </div>
+          <div className="row">
+            <Button> 4 </Button>
+            <Button> 5 </Button>
+            <Button> 6 </Button>
+            <Button> * </Button>
+          </div>
+          <div className="row">
+            <Button> 1 </Button>
+            <Button> 2 </Button>
+            <Button> 3 </Button>
+            <Button> - </Button>
+          </div>
+          <div className="row">
+            <Button> 0 </Button>
+            <Button> . </Button>
+            <Button> = </Button>
+            <Button> + </Button>
+          </div>
         </div>
       </div>
     );
