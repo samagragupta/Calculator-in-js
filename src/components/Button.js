50866e39d5541e43c169e9ae9278344e9ae091e8
samagragupta <samagraguptajpr@gmail.com>
2018-11-14 21:19:00 +0530

    Complete Calculator

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,23 +9,3 @@ export const Button = props => (
     {props.children}
   </div>
 );
-
-
-//
-// import React from "react";
-// import "./Button.css";
-//
-// const isOperator = val => {
-//   return !isNaN(val) || val === "." || val === "=";
-// };
-//
-// export const Button = props => (
-//   <div
-//     className={`button-wrapper ${
-//       isOperator(props.children) ? null : "operator"
-//     }`}
-//     onClick={() => props.handleClick(props.children)}
-//   >
-//     {props.children}
-//   </div>
-// );
